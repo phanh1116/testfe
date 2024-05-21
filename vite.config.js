2f2685a8381f3f68b0d2a587697b3df1b7228a9d
panh20020123 <tranpanh55@gmail.com>
2024-05-21 07:01:55 +0700

    deploy 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()], // Use the imported plugin function here
+  base: "/testfe",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
